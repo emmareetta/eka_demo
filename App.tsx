0f1f473878afbbbb04dd236b102f894ab84e0274
Emma-Reetta Leino <emma@leino.org>
2023-08-29 21:53:09 +0300

    tehtävä 1 valmis

--- App.tsx
+++ App.tsx
@@ -1,11 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [luku1, setLuku1] = useState("");
+  const [luku2, setLuku2] = useState("");
+
+  const [result, setResult] = useState<number | undefined>(undefined);
+
+  const laskeYhteen = () => {
+    setResult(parseInt(luku1) + parseInt(luku2)) 
+  }
+
+  const laskeErotus = () => {
+    setResult(parseInt(luku1) - parseInt(luku2)) 
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Result: {result}</Text>
+      <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={text => setLuku1(text)} value = {luku1} inputMode='numeric'/>
+      <TextInput style={{width: 200, borderColor: 'gray', borderWidth: 1}}
+      onChangeText={text => setLuku2(text)} value = {luku2} inputMode='numeric'/>
+      <View style={{width: 100, flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between'}}>
+        <Button
+          onPress={laskeYhteen}
+          title="+"
+          />
+        <Button
+          onPress={laskeErotus}
+          title="-"
+          />
+      </View>
+      
+     
     </View>
   );
 }
@@ -18,3 +47,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
